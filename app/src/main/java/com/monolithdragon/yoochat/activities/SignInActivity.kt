9becf65250d421860e1530cac53f4866ffb5cc56
monolithdragon <joo.lajos76@gmail.com>
2022-10-22 23:29:03 +0200

    Added firebase authentication with email and password

--- app/src/main/java/com/monolithdragon/yoochat/activities/SignInActivity.kt
+++ app/src/main/java/com/monolithdragon/yoochat/activities/SignInActivity.kt
@@ -3,10 +3,17 @@ package com.monolithdragon.yoochat.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.monolithdragon.yoochat.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,12 +21,85 @@ class SignInActivity : AppCompatActivity() {
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initFields()
         setListener()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        
+        if(auth.currentUser != null) {
+            switchToMainActivity()
+        }
+    }
+
+    private fun initFields() {
+        auth = Firebase.auth
     }
 
     private fun setListener() {
         binding.textHaveNewAccount.setOnClickListener {
             switchToSignUpActivity()
+        }
+        
+        binding.buttonSignIn.setOnClickListener { 
+            if (isValidate()) {
+                signIn()
+            }
+        }
+    }
+
+    private fun isValidate(): Boolean {
+        val email = binding.inputEmail.text.toString()
+        val password = binding.inputPassword.text.toString()
+
+        if (email.trim().isEmpty()) {
+            showMessage("Enter email address...")
+            return false
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            showMessage("Enter valid email address...")
+            return false
+        }
+
+        if (password.trim().isEmpty()) {
+            showMessage("Enter password...")
+            return false
+        }
+
+        return true
+    }
+
+    private fun signIn() {
+        val email = binding.inputEmail.text.toString()
+        val password = binding.inputPassword.text.toString()
+
+        loaded(true)
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    loaded(false)
+                    switchToMainActivity()
+                } else {
+                    loaded(false)
+                    showMessage("Authentication failed.")
+                }
+            }
+    }
+    
+    private fun showMessage(message: String) {
+        Toast.makeText(this@SignInActivity, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun loaded(enabled: Boolean) {
+        if (enabled) {
+            binding.buttonSignIn.visibility = View.INVISIBLE
+            binding.progressBarSignIn.visibility = View.VISIBLE
+        } else {
+            binding.progressBarSignIn.visibility = View.INVISIBLE
+            binding.buttonSignIn.visibility = View.VISIBLE
         }
     }
 
@@ -29,5 +109,10 @@ class SignInActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    private fun switchToMainActivity() {
+        val intent = Intent(this@SignInActivity, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
 
 }
--- app/src/main/java/com/monolithdragon/yoochat/activities/SignUpActivity.kt
+++ app/src/main/java/com/monolithdragon/yoochat/activities/SignUpActivity.kt
@@ -1,12 +1,26 @@
 package com.monolithdragon.yoochat.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Base64
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.monolithdragon.yoochat.R
 import com.monolithdragon.yoochat.databinding.ActivitySignUpBinding
+import java.io.ByteArrayOutputStream
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
+
+    private var encodedProfileImage: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,17 +28,122 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initFields()
         setListeners()
+    }
+
+    private fun initFields() {
+        auth = Firebase.auth
     }
 
     private fun setListeners() {
         binding.textAlreadyHaveAccount.setOnClickListener {
-            switchToSignUpActivity()
+            switchToSignInActivity()
+        }
+
+        binding.buttonSignUp.setOnClickListener {
+            if (isValidate()) {
+                signUp()
+            }
         }
     }
 
-    private fun switchToSignUpActivity() {
+    private fun signUp() {
+        val email = binding.inputEmail.text.toString()
+        val password = binding.inputPassword.text.toString()
+
+        loaded(true)
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    loaded(false)
+                    switchToMainActivity()
+                } else {
+                    loaded(false)
+                    showMessage("Authorization failed")
+                }
+            }
+    }
+
+    private fun isValidate(): Boolean {
+       val name = binding.inputName.text.toString()
+       val email = binding.inputEmail.text.toString()
+       val password = binding.inputPassword.text.toString()
+       val confirmedPassword = binding.inputConfirmPassword.text.toString()
+
+
+        if (encodedProfileImage.isNullOrEmpty()) {
+            encodedProfileImage =
+                encodeImage(BitmapFactory.decodeResource(resources, R.drawable.profile_image))
+        }
+
+        if (name.trim().isEmpty()) {
+            showMessage("Enter username...")
+            return false
+        }
+
+        if (email.trim().isEmpty()) {
+            showMessage("Enter email address...")
+            return false
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            showMessage("Enter valid email address...")
+            return false
+        }
+
+        if (password.trim().isEmpty()) {
+            showMessage("Enter password...")
+            return false
+        }
+
+        if (confirmedPassword.trim().isEmpty()) {
+            showMessage("Enter confirmed password...")
+            return false
+        }
+
+        if (confirmedPassword != password) {
+            showMessage("Password not matched...")
+            return false
+        }
+
+        return true
+    }
+
+    private fun encodeImage(bitmap: Bitmap): String {
+        val previewWidth = 150
+        val previewHeight = bitmap.height * previewWidth / bitmap.width
+        val previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, true)
+        val outputStream = ByteArrayOutputStream()
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
+        val bytes = outputStream.toByteArray()
+
+        return Base64.encodeToString(bytes, Base64.DEFAULT)
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this@SignUpActivity, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun loaded(enabled: Boolean) {
+        if (enabled) {
+            binding.buttonSignUp.visibility = View.INVISIBLE
+            binding.progressBarSignUp.visibility = View.VISIBLE
+        } else {
+            binding.progressBarSignUp.visibility = View.INVISIBLE
+            binding.buttonSignUp.visibility = View.VISIBLE
+        }
+    }
+
+    private fun switchToSignInActivity() {
         val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+
+    private fun switchToMainActivity() {
+        val intent = Intent(this@SignUpActivity, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
     }
