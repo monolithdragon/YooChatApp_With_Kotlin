9f339f8ee553253e4b3d43d3b22c8fd676b7b66c
monolithdragon <joo.lajos76@gmail.com>
2022-10-22 21:36:25 +0200

    Update with binding and text click listener

--- app/src/main/java/com/monolithdragon/yoochat/SignInActivity.kt
+++ app/src/main/java/com/monolithdragon/yoochat/SignInActivity.kt
@@ -1,11 +1,33 @@
 package com.monolithdragon.yoochat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.monolithdragon.yoochat.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListener()
     }
+
+    private fun setListener() {
+        binding.textHaveNewAccount.setOnClickListener {
+            switchToSignUpActivity()
+        }
+    }
+
+    private fun switchToSignUpActivity() {
+        val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+
+
 }
--- app/src/main/java/com/monolithdragon/yoochat/SignUpActivity.kt
+++ app/src/main/java/com/monolithdragon/yoochat/SignUpActivity.kt
@@ -1,11 +1,31 @@
 package com.monolithdragon.yoochat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.monolithdragon.yoochat.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.textAlreadyHaveAccount.setOnClickListener {
+            switchToSignUpActivity()
+        }
+    }
+
+    private fun switchToSignUpActivity() {
+        val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
     }
 }
