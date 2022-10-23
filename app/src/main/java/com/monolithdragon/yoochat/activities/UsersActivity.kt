4259c5dbce6997ada678e8a43d99a4937abc2399
monolithdragon <joo.lajos76@gmail.com>
2022-10-23 14:59:27 +0200

    Added users that selectable in application

---
+++ app/src/main/java/com/monolithdragon/yoochat/activities/UsersActivity.kt
@@ -0,0 +1,104 @@
+package com.monolithdragon.yoochat.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.monolithdragon.yoochat.R
+import com.monolithdragon.yoochat.adapters.UserAdapter
+import com.monolithdragon.yoochat.databinding.ActivityUsersBinding
+import com.monolithdragon.yoochat.models.User
+import com.monolithdragon.yoochat.utilities.Constants
+import com.monolithdragon.yoochat.utilities.PreferenceManager
+
+class UsersActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUsersBinding
+    private lateinit var database: FirebaseFirestore
+    private lateinit var preferenceManager: PreferenceManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUsersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initFields()
+        setListeners()
+        setUserDataFromDatabase()
+    }
+
+    private fun initFields() {
+        database = Firebase.firestore
+        preferenceManager = PreferenceManager(this@UsersActivity)
+    }
+
+    private fun setListeners() {
+        binding.imageBack.setOnClickListener {
+            switchBackToMainActivity()
+        }
+    }
+
+    private fun setUserDataFromDatabase() {
+        loaded(true)
+
+        database.collection(Constants.KEY_COLLECTION_USERS)
+            .get()
+            .addOnSuccessListener { documents ->
+                val currentUserId = preferenceManager.getString(Constants.KEY_USER_ID)
+                val users: MutableList<User> = mutableListOf()
+
+                for (document in documents) {
+                    if (currentUserId == document.id) {
+                        continue
+                    }
+
+                    val user = User()
+                    user.id = document.id
+                    user.name = document.getString(Constants.KEY_USER_NAME)
+                    user.email = document.getString(Constants.KEY_USER_EMAIL)
+                    user.profileImage = document.getString((Constants.KEY_USER_IMAGE))
+                    user.token = document.getString(Constants.KEY_USER_TOKEN)
+
+                    users.add(user)
+                }
+
+                if (users.isNotEmpty()) {
+                    users.sortWith { x, y -> x.name!!.compareTo(y.name!!) }
+
+                    val adapter = UserAdapter(users)
+                    binding.usersRecycleView.adapter = adapter
+                    binding.usersRecycleView.visibility = View.VISIBLE
+                } else {
+                    showErrorMessage()
+                }
+
+                loaded(false)
+            }
+            .addOnFailureListener { exception ->
+                loaded(false)
+                showErrorMessage()
+            }
+    }
+
+    private fun loaded(enabled: Boolean) {
+        if (enabled) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun showErrorMessage() {
+        binding.textErrorMessage.text = String.format("%s", getString(R.string.no_user_available))
+        binding.textErrorMessage.visibility = View.VISIBLE
+    }
+
+    private fun switchBackToMainActivity() {
+        val intent = Intent(this@UsersActivity, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/monolithdragon/yoochat/adapters/UserAdapter.kt
@@ -0,0 +1,44 @@
+package com.monolithdragon.yoochat.adapters
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.monolithdragon.yoochat.databinding.ItemContainerUserBinding
+import com.monolithdragon.yoochat.models.User
+
+class UserAdapter(private var users: List<User>) :
+        RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    inner class UserViewHolder(private val binding: ItemContainerUserBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            binding.textName.text = user.name
+            binding.textEmail.text = user.email
+            binding.imageProfile.setImageBitmap(getUserImage(user.profileImage))
+        }
+
+        private fun getUserImage(encodedImage: String?): Bitmap {
+            val bytes = Base64.decode(encodedImage, Base64.DEFAULT)
+            return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view = ItemContainerUserBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(users[position])
+    }
+
+    override fun getItemCount(): Int = users.size
+
+}
