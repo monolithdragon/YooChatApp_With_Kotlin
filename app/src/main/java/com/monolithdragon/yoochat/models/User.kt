b4869755c1bd91addea4381b17729dc9dee73669
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2022-10-23 15:03:10 +0200

    Merge pull request #2
    * Update with firebase messaging
    
    * Added token to authorized user
    
    * Added users that selectable in application

---
+++ app/src/main/java/com/monolithdragon/yoochat/models/User.kt
@@ -0,0 +1,42 @@
+package com.monolithdragon.yoochat.models
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class User(
+    var id: String? = null,
+    var name: String? = null,
+    var email: String? = null,
+    var profileImage: String? = null,
+    var token: String? = null
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(name)
+        parcel.writeString(email)
+        parcel.writeString(profileImage)
+        parcel.writeString(token)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
